d2d7c250f6caefd99017f609915c12d9824b05b1
Nonthachai Plodthong <nonthachaiplodthong@Nonthachais-MacBook-Pro.local>
2023-11-27 16:47:20 -0500

    Debug AWS

--- src/model/data/index.js
+++ src/model/data/index.js
@@ -1,7 +1,9 @@
 // src/model/data/index.js
 
+const logger = require('../../logger');
+
 // If the env sets an AWS REGION, use that
 // service s S3 DynamoDB other we will use the in-memory db
-
+logger.info(`[DATA] Using ${process.env.AWS_REGION ? 'AWS' : 'memory'} data store`);
 module.exports = process.env.AWS_REGION ? require('./aws') : require('./memory');
 
